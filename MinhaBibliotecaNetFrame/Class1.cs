ffca18e626fb2d12e7d2ef2aafb6d13a56e92564
tobiasgoncalvespires <tobiasgp@gmail.com>
2018-11-24 14:43:29 -0200

    enviando projeto .net inteiro

---
+++ MinhaBibliotecaNetFrame/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaBibliotecaFrame
+{
+    public class MinhaClasseFrame
+    {
+
+        public static string rodar()
+        {
+            return "Retorno Interno";
+        }
+
+    }
+}
+
+
